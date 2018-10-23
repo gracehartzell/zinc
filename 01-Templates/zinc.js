43ad56cbaa961914576d5046b29d01ac40ec0658
Grace Hartzell <gracehartzell@utexas.edu>
2018-10-23 13:31:42 -0500

    Creating elements with templates completed

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -5,47 +5,37 @@
 (() => {
     function populateList(results) {
 
-        const container = document.createElement('div');
-        document.body.appendChild(container);
-        container.setAttribute('class', 'container');
-       
-        const h1 = document.createElement('h1');
-        container.appendChild(h1);
-        h1.innerHTML = 'User List';
-
-        const userUl = document.createElement('ul');
-        container.appendChild(userUl);
-        userUl.setAttribute('id', 'z-user-list');
-        
         for (let result in results) {
-
-            const newUser = document.createElement('li');
-            newUser.setAttribute('class', 'user');
-            userUl.appendChild(newUser);
-    
-            const userPhoto = document.createElement('img');
-            userPhoto.setAttribute('class', 'user-photo');
-            userPhoto.setAttribute('src', results[result].picture.large)
-            newUser.appendChild(userPhoto);
-    
-            const userName = document.createElement('div');
-            userName.setAttribute('class', 'user-name');
-            userName.innerHTML = `${results[result].name.first} ${results[result].name.last}`
-            newUser.appendChild(userName);
-    
-            const userLocation = document.createElement('div');
-            userLocation.setAttribute('class', 'user-location');
-            userLocation.innerHTML = `${results[result].location.city}, ${results[result].location.state}`
-            newUser.appendChild(userLocation);
-    
-            const userEmail = document.createElement('div');
-            userEmail.setAttribute('class', 'user-email');
-            userEmail.innerHTML = `${results[result].email}`
-            newUser.appendChild(userEmail);
+            const user = {
+                photo: results[result].picture.large,
+                firstName: results[result].name.first,
+                lastName: results[result].name.last,
+                city: results[result].location.city,
+                state: results[result].location.state,
+                email: results[result].email
+            };
+            renderTemplate(user);
         }
-
         console.log(results); // eslint-disable-line no-console
     }
+
+    
+
+
+    function renderTemplate(user) {
+        let userUl = document.getElementById('z-user-list');
+
+        const markup = `
+          <li class="user">
+            <img class="user-photo" src="${ user.photo }" alt="Photo of ${ user.firstName } ${ user.lastName }">
+            <div class="user-name">${ user.firstName } ${ user.lastName }</div>
+            <div class="user-location">${ user.city }, ${ user.state }</div>
+            <div class="user-email">${ user.email }</div>
+          </li>
+        `
+        userUl.insertAdjacentHTML('beforeend', markup);
+    }
+
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
