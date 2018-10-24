a7605bd50cd37d3b5bfc2b7a9ed3c0b6d84e5799
Grace Hartzell <gracehartzell@utexas.edu>
2018-10-24 08:56:39 -0500

    Redo of Part 2 with regex

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -4,44 +4,54 @@
 
 (() => {
     function populateList(results) {
-
-        for (let result in results) {
-            const user = {
-                photo: results[result].picture.large,
-                firstName: results[result].name.first,
-                lastName: results[result].name.last,
-                city: results[result].location.city,
-                state: results[result].location.state,
-                email: results[result].email
+        results.forEach(user => {
+            const userObj = {
+                photo: user.picture.large,
+                firstName: user.name.first,
+                lastName: user.name.last,
+                city: user.location.city,
+                state: user.location.state,
+                email: user.email
             };
-            renderTemplate(user);
-        };
-        console.log(results); // eslint-disable-line no-console
+            renderTemplate(userObj);
+        });
     };
 
-    function renderTemplate(user) {
+    function renderTemplate(userObj) {
         let userUl = document.getElementById('z-user-list');
 
         const markup = `
           <li class="user">
-            <img class="user-photo" src="${ user.photo }" alt="Photo of ${ user.firstName } ${ user.lastName }">
-            <div class="user-name">${ user.firstName } ${ user.lastName }</div>
-            <div class="user-location">${ user.city }, ${ user.state }</div>
-            <div class="user-email">${ user.email }</div>
+            <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+            <div class="user-name">{{ firstName }} {{ lastName }}</div>
+            <div class="user-location">{{ city }}, {{ state }}</div>
+            <div class="user-email">{{ email }}</div>
           </li>
-        `;
-        
-        userUl.insertAdjacentHTML('beforeend', markup);
-    };
+        `
+        const regEx = /{{\s*([\w.]+)\s*}}/g;
 
+        const matches = markup.match(regEx);
+
+        let completeMarkup = markup; // <-- Copy
+
+        matches.forEach(match => {
+            const temp1 = match.replace(/{{\W/g, '');
+            const key = temp1.replace(/\W}}/g, '');
+            completeMarkup = completeMarkup.replace(match, userObj[key] || '')
+        })
+
+        console.warn('completeMarkup', completeMarkup);
+
+        userUl.insertAdjacentHTML('beforeend', completeMarkup);
+    };
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
             .then(res => res.json())
             .then(json => populateList(json.results));
-    };
+    }
 
     document.addEventListener('DOMContentLoaded', init);
 
-
 })();
+
