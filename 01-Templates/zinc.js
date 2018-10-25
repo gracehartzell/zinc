546aae1c6664d0884d4d50150267913395679d5f
Grace Hartzell <gracehartzell@utexas.edu>
2018-10-25 09:54:05 -0500

    Completed, refactored, and added notes

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -2,63 +2,55 @@
 
 /* eslint-env browser */
 
+/*
+    0. The problem is loading an HTML file into the renderTemplate function so that
+        the template will look the same but it will NOT be the index.html file 
+        (instead will be the user.html). So the call for each new user list 
+        will look like renderTemplate('user', user).
+    1. In order to do this, you have to run a simple server 
+        (used python -m SimpleHTTPServer, can also do `npm install -g lite-server`), otherwise you'll get a CORS error.
+    2. Remove extraneous information (namely the template string since we don't
+        need that anymore!) OR BETTER YET, REMOVE EVERYTHING!
+    3. Add in a fetch to get the appropriate HTML file. 
+        3a. Since we'll now be dealing with HTML and not the template string, 
+            we need to make sure that all references to the template string are 
+            instead referencing the HTML file. 
+        3b. Instead of using the "normal"/usual method of response.json(), we 
+            need to use the .text() method so that it converts the byte stream 
+            into plain text which can then be parsed as HTML by the browser.
+        3c. Once we have the HTML/data, we want to take that and apply it to the 
+            template (this part is pretty much identical to what we did in part 3).
+
+    4. REFACTORING NOTE: I ended up removing the populateList() because it became 
+        somewhat unnecessary and I felt as though I could do the same thing with
+        the same readability by putting what was in that function into the 
+        renderTemplate function. 
+*/
+
 (() => {
+    const userList = document.getElementById('z-user-list');
 
-    const userTemplate = 
-      `<li class="user">
-        <img class="user-photo" src="{{ picture.large }}" 
-            alt="Photo of {{ name.first }} {{ name.last }}">
-        <div class="user-name">{{ name.first }} {{ name.last }}</div>
-        <div class="user-location">{{ location.city }}, {{ location.state }}</div>
-        <div class="user-email">{{ email }}</div>
-      </li>`
-
-    function renderTemplate(templateString, data) {
-        return templateString.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, p1) => 
-            p1.split('.').reduce((acc, curr) => acc[curr], data)
-        );
-    }
-
-    /*
-    renderTemplate explained:
-    0. The regex that I used will work; however, when there is a variable name with a 
-        non-alphanumeric character, it won't capture it. The better way to do it to 
-        fully capture the group would be: (/\{\{\s*(.*?)\s*\}\}/g).
-    1. p1 is the captured group (words) within the regex (aka the words)
-    2. split('.') returns an array of the values within p1 
-        (e.g. p1 = picture.large returns ['picture', 'large'])
-    3. reduce is used to apply the function to each element in the array to reduce it to a 
-        single value and return something new. This array will give the keys for the 
-        object to be applied to the returned data. If the desired data is nested, the 
-        function will be able to grab the second keys to get further into the object.
-            3a. Passing data in as the starting point for the function. 
-            3b. Example case if item were name.first -> 
-                The split('.') would first break it into ['name', 'first']
-                next, reduce would take over and produce:
-                _________acc__|__curr___
-                        data  | 'name'
-                   data.name  | 'first'
-             data.name.first  |  --
-
-    */
-
-    function populateList(results) {
-
-        const userList = document.getElementById('z-user-list');
-
-        results.forEach((user) => {
-            const html = renderTemplate(userTemplate, user);
-            userList.insertAdjacentHTML('beforeend', html);
-        });
+    function renderTemplate(template, users) {
+        return fetch(`${template}.html`)
+            .then(template => template.text())
+            .then(template => {
+            
+              users.forEach((user) => {
+                let renderedTemplate = template.replace(/\{\{\s*(.*?)\s*\}\}/g, 
+                    (match, p1) => {
+                        return p1.split('.').reduce((acc, curr) => acc[curr], user) || '';
+                    });
+                userList.insertAdjacentHTML('beforeend', renderedTemplate)
+              });
+            });
     };
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
             .then(res => res.json())
-            .then(json => populateList(json.results));
+            .then(data => renderTemplate('user', data.results));
     }
 
     document.addEventListener('DOMContentLoaded', init);
 
 })();
-
