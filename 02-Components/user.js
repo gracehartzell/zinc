288f8e59e96602d48a265c7be12d1f535872db12
Grace Hartzell <gracehartzell@utexas.edu>
2018-10-29 10:51:09 -0500

    Completed part 2; needs refactoring

--- 02-Components/user.js
+++ 02-Components/user.js
@@ -4,6 +4,7 @@
 /* globals Zinc */
 
 (() => {
+
     const userData = {
         picture: {
             thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
@@ -18,4 +19,29 @@
         },
         email: 'jack.burton@example.com'
     };
+
+    const userData2 = {
+        picture: {
+            thumbnail: 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54ced09f20bf3_-_esq-bluth-xlg.jpg'
+        },
+        name: {
+            first: 'Lucille',
+            last: 'Bluthe'
+        },
+        location: {
+            city: 'Sudden Valley',
+            state: 'CA'
+        },
+        email: 'best.mom@example.com'
+    };
+
+
+
+    /*
+        • Get the element
+        • Get the template
+        • Render template with data
+        • Insert template into element
+     */
+
 })();
--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -2,58 +2,38 @@
 
 /* eslint-env browser */
 
-/*
-    0. To start off, this is extremely similar to the 4th part of templates
-        a. Use the user.js file for things you want on the page and 
-           this file (zinc.js) for anything you want to reuse, so put all of 
-           the rendering and work into this one. I think?
-        b. We still want to use the user.html file and in it, we need to do 
-           the same extraction that we'd done with the previous exercise. 
-           In order to do this, we need to add in the renderTemplate function
-           that we've already created.
-    1. Element is 'user-item' (you can see that on the index.html page), content
-         is 'user' (AKA the template file, just like how we did it earlier). 
-*/
-
-
 const Zinc = {};
 
 (() => {
-    const userData = {
-        picture: {
-            thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
-        },
-        name: {
-            first: 'Jack',
-            last: 'Burton'
-        },
-        location: {
-            city: 'San Francisco',
-            state: 'CA'
-        },
-        email: 'jack.burton@example.com'
-    };
 
-    function renderComponent(element, templateFile, data) {
+    Zinc.registerComponent = function (elementName, templateFile, dataObject) {
+        let element = document.querySelector(elementName);
 
+        renderTemplate(templateFile, dataObject)
+            .then((result) => {
+                element.insertAdjacentHTML('beforeend', result)
+            });
+
+    }
+
+    function renderTemplate(templateFile, dataObject) {
         return fetch(`${templateFile}.html`)
-        .then(res => res.text())
-        .then(template => {
-            const el = document.querySelector(element);
-            
-            let renderedTemplate = template.replace(/\{\{\s*(.*?)\s*\}\}/g, 
-                (match, p1) => {
-                    return p1.split('.').reduce((acc, curr) => acc[curr], data) || '';
-                });
-            
-            el.insertAdjacentHTML('beforeend', renderedTemplate)
+            .then(res => res.text())
+            .then(template =>  
+                template.replace(/\{\{\s*(.*?)\s*\}\}/g, (match, p1) =>
+                    p1.split('.').reduce((acc, curr) => acc[curr], dataObject) || '' ));
+    }
 
-        });
-    };
-    
     function init() {
-        renderComponent('user-item', 'user', userData);
+        fetch('https://randomuser.me/api/?results=5')
+            .then(res => res.json())
+            .then(res =>
+                res.results.forEach(result => {
+                    Zinc.registerComponent('user-item', 'user', result)
+                })
+            )
     }
 
     document.addEventListener('DOMContentLoaded', init);
+
 })();
